647d8c5fe4d2ccfe6a58fb025ddcfe7f2cb12a2f
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-04 05:14:55 -0700

    Update purge.rs

--- cargio-db-dev/src/subcommands/purge_signatures/purge.rs
+++ cargio-db-dev/src/subcommands/purge_signatures/purge.rs
@@ -3,9 +3,9 @@ use std::{
     path::Path,
 };
 
-use casper_hashing::Digest;
-use casper_node::types::{BlockHash, BlockHeader};
-use casper_types::{EraId, ProtocolVersion, PublicKey, U512};
+use cargio_hashing::Digest;
+use master_node::types::{BlockHash, BlockHeader};
+use cargio_types::{EraId, ProtocolVersion, PublicKey, U512};
 use lmdb::{Cursor, Database, Environment, Error as LmdbError, Transaction, WriteFlags};
 use log::{error, info, warn};
 
@@ -17,19 +17,13 @@ use crate::common::{
 
 use super::{block_signatures::BlockSignatures, signatures::strip_signatures, Error};
 
-/// Structure to hold lookup information for a set of block headers.
 #[derive(Default)]
 pub(crate) struct Indices {
-    /// Hold the hash and the header of a block keyed by its height.
     pub(crate) heights: BTreeMap<u64, (BlockHash, BlockHeader)>,
-    /// Hold the hash of switch blocks keyed by the era for which they hold
-    /// the weights.
     pub(crate) switch_blocks: BTreeMap<EraId, BlockHash>,
-    /// Hold the heights of switch blocks before upgrades.
     pub(crate) switch_blocks_before_upgrade: BTreeSet<u64>,
 }
 
-/// Cache-like structure to store the validator weights for an era.
 #[derive(Default)]
 pub(crate) struct EraWeights {
     era_id: EraId,
@@ -38,11 +32,6 @@ pub(crate) struct EraWeights {
 }
 
 impl EraWeights {
-    /// Update the internal structure to hold the validator weights for
-    /// the era given as input.
-    ///
-    /// Return value is `true` when the switch block used to populate the
-    /// weights is a switch block right before an upgrade or `false` otherwise.
     pub(crate) fn refresh_weights_for_era<T: Transaction>(
         &mut self,
         txn: &T,
@@ -50,24 +39,19 @@ impl EraWeights {
         indices: &Indices,
         era_id: EraId,
     ) -> Result<bool, Error> {
-        // If we already have the requested era, exit early.
         if self.era_id == era_id {
             return Ok(self.era_after_upgrade);
         }
-        // Get the required era's associated switch block.
         let switch_block_hash = indices
             .switch_blocks
             .get(&era_id)
             .ok_or_else(|| Error::MissingEraWeights(era_id))?;
-        // Deserialize it.
         let switch_block_header: BlockHeader =
             bincode::deserialize(txn.get(db, &switch_block_hash)?)
                 .map_err(|bincode_err| Error::HeaderParsing(*switch_block_hash, bincode_err))?;
-        // Check if this switch block is the last in the era before an upgrade.
         self.era_after_upgrade = indices
             .switch_blocks_before_upgrade
             .contains(&switch_block_header.height());
-        // Get the weights.
         let weights = switch_block_header
             .next_era_validator_weights()
             .cloned()
@@ -88,8 +72,6 @@ impl EraWeights {
     }
 }
 
-/// Creates a collection of indices to store lookup information for a given
-/// list of block heights.
 pub(crate) fn initialize_indices(
     env: &Environment,
     needed_heights: &BTreeSet<u64>,
@@ -114,13 +96,11 @@ pub(crate) fn initialize_indices(
 
     {
         let mut last_blocks_before_upgrade: BTreeMap<ProtocolVersion, u64> = BTreeMap::default();
-        // Iterate through all block headers.
         let mut cursor = txn.open_ro_cursor(header_db)?;
         for (raw_key, raw_value) in cursor.iter() {
             if let Some(progress_tracker) = maybe_progress_tracker.as_mut() {
                 progress_tracker.advance_by(1);
             }
-            // Deserialize the block hash.
             let block_hash: BlockHash = match Digest::try_from(raw_key) {
                 Ok(digest) => digest.into(),
                 Err(digest_parsing_err) => {
@@ -128,18 +108,13 @@ pub(crate) fn initialize_indices(
                     continue;
                 }
             };
-            // Deserialize the header.
             let block_header: BlockHeader = bincode::deserialize(raw_value)
                 .map_err(|bincode_err| Error::HeaderParsing(block_hash, bincode_err))?;
             let block_height = block_header.height();
-            // We store all switch block hashes keyed by the era for which they
-            // hold the weights.
             if block_header.is_switch_block() {
                 let _ = indices
                     .switch_blocks
                     .insert(block_header.era_id().successor(), block_hash);
-                // Store the highest switch block height for each protocol
-                // version we encounter.
                 match last_blocks_before_upgrade.entry(block_header.protocol_version()) {
                     Entry::Vacant(vacant_entry) => {
                         vacant_entry.insert(block_height);
@@ -151,10 +126,6 @@ pub(crate) fn initialize_indices(
                     }
                 }
             }
-            // If this block is on our list, store its hash and header in the
-            // indices. We store the header to avoid looking it up again in the
-            // future since we know we will need it and we expect
-            // `needed_heights` to be a relatively small list.
             if needed_heights.contains(&block_height)
                 && indices
                     .heights
@@ -164,10 +135,7 @@ pub(crate) fn initialize_indices(
                 return Err(Error::DuplicateBlock(block_height));
             };
         }
-        // Remove the entry for the highest known protocol version as it hasn't
-        // had an upgrade yet.
         let _ = last_blocks_before_upgrade.pop_last();
-        // Store the heights of the relevant switch blocks in the indices.
         indices
             .switch_blocks_before_upgrade
             .extend(last_blocks_before_upgrade.into_values());
@@ -176,17 +144,6 @@ pub(crate) fn initialize_indices(
     Ok(indices)
 }
 
-/// Purges finality signatures from a database for all blocks of heights found
-/// in `heights_to_visit`.
-///
-/// If the `full_purge` flag is set, all the signatures for the associated
-/// block will be purged by deleting the record in the block signatures
-/// database.
-///
-/// If the `full_purge` flag is not set, signatures will be purged until the
-/// remaining set of signatures gives the block weak but not strict finality.
-/// If this is not possible for that block given its signature set and the era
-/// weights, it is skipped and a message is logged.
 pub(crate) fn purge_signatures_for_blocks(
     env: &Environment,
     indices: &Indices,
@@ -220,10 +177,8 @@ pub(crate) fn purge_signatures_for_blocks(
     .map_err(|_| Error::EmptyBlockList)?;
 
     for height in heights_to_visit {
-        // Get the block hash and header from the indices for this height.
         let (block_hash, block_header) = match indices.heights.get(&height) {
             Some((block_hash, block_header)) => {
-                // We don't strip signatures for the genesis block.
                 if block_header.era_id().is_genesis() {
                     warn!("Cannot strip signatures for genesis block");
                     progress_tracker.advance_by(1);
@@ -232,7 +187,6 @@ pub(crate) fn purge_signatures_for_blocks(
                 (block_hash, block_header)
             }
             None => {
-                // Skip blocks which are not in the database.
                 warn!("Block at height {height} is not present in the database");
                 progress_tracker.advance_by(1);
                 continue;
@@ -240,8 +194,6 @@ pub(crate) fn purge_signatures_for_blocks(
         };
         let block_height = block_header.height();
         let era_id = block_header.era_id();
-        // Make sure we have the correct era weights for this block before
-        // trying to strip any signatures.
         let era_after_upgrade =
             era_weights.refresh_weights_for_era(&txn, header_db, indices, era_id)?;
 
@@ -249,7 +201,6 @@ pub(crate) fn purge_signatures_for_blocks(
             Ok(raw_signatures) => bincode::deserialize(raw_signatures)
                 .map_err(|bincode_err| Error::SignaturesParsing(*block_hash, bincode_err))?,
             Err(LmdbError::NotFound) => {
-                // Skip blocks which have no signature entry in the database.
                 warn!(
                     "No signature entry in the database for block \
                     {block_hash} at height {block_height}"
@@ -261,7 +212,6 @@ pub(crate) fn purge_signatures_for_blocks(
         };
 
         if full_purge {
-            // Delete the record completely from the database.
             txn.del(signatures_db, &block_hash, None)?;
         } else if strip_signatures(&mut block_signatures, &era_weights.weights) {
             if era_after_upgrade {
@@ -270,8 +220,6 @@ pub(crate) fn purge_signatures_for_blocks(
                     for block {block_hash} at height {block_height}"
                 );
             }
-            // Serialize the remaining signatures and overwrite the database
-            // entry.
             let serialized_signatures = bincode::serialize(&block_signatures)
                 .map_err(|bincode_err| Error::Serialize(*block_hash, bincode_err))?;
             txn.put(
