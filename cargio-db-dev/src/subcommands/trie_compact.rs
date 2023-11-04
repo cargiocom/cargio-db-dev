435b49f7427e6843d9ccaf612b7e80f032c209bf
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-04 05:38:42 -0700

    Update trie_compact.rs

--- cargio-db-dev/src/subcommands/trie_compact.rs
+++ cargio-db-dev/src/subcommands/trie_compact.rs
@@ -2,8 +2,6 @@ mod compact;
 mod helpers;
 #[cfg(test)]
 pub(crate) mod tests;
-// All code in the `utils` mod was copied from `casper-node` because it isn't available in the
-// public interface.
 mod utils;
 
 use std::{io::Error as IoError, path::PathBuf};
@@ -13,8 +11,8 @@ use clap::{Arg, ArgMatches, Command};
 use lmdb::Error as LmdbError;
 use thiserror::Error as ThisError;
 
-use casper_hashing::Digest;
-use casper_node::storage::Error as StorageError;
+use cargio_hashing::Digest;
+use master_node::storage::Error as StorageError;
 
 use compact::DestinationOptions;
 pub use helpers::copy_state_root;
@@ -29,34 +27,24 @@ pub const DEFAULT_MAX_DB_SIZE: &str = "483183820800"; // 450 gb
 const SOURCE_TRIE_STORE_PATH: &str = "src-trie";
 const STORAGE_PATH: &str = "storage-path";
 
-/// Possible errors caught while compacting the trie store.
 #[derive(Debug, ThisError)]
 pub enum Error {
-    /// Error copying the state root with a specific digest.
     #[error("Error copying state root {0}: {1}")]
     CopyStateRoot(Digest, AnyError),
-    /// Error creating the execution engine for the destination trie.
     #[error("Error loading the execution engine: {0}")]
     CreateDestTrie(AnyError),
-    /// Error working with the destination trie path.
     #[error("Invalid destination: {0}")]
     InvalidDest(String),
-    /// Path cannot be created/resolved.
     #[error("Path {0} cannot be created/resolved: {1}")]
     InvalidPath(PathBuf, IoError),
-    /// Error while operating on LMDB.
     #[error("Error while operating on LMDB: {0}")]
     LmdbOperation(LmdbError),
-    /// A block of specific height is missing from the storage.
     #[error("Storage database is missing block {0}")]
     MissingBlock(u64),
-    /// Error creating the execution engine for the source trie.
     #[error("Error creating the execution engine: {0}")]
     OpenSourceTrie(AnyError),
-    /// Error opening the block/deploys LMDB store.
     #[error("Error opening the block/deploy storage: {0}")]
     OpenStorage(AnyError),
-    /// Error while getting a block of specific height from storage.
     #[error("Storage error while trying to retrieve block {0}: {1}")]
     Storage(u64, StorageError),
 }
@@ -153,7 +141,6 @@ pub fn run(matches: &ArgMatches) -> Result<(), Error> {
     let storage_path = matches.value_of(STORAGE_PATH).unwrap();
     let source_trie_path = matches.value_of(SOURCE_TRIE_STORE_PATH).unwrap();
     let destination_trie_path = matches.value_of(DESTINATION_TRIE_STORE_PATH).unwrap();
-    // Prettier than C style if/else.
     let dest_opt = match matches {
         _ if matches.is_present(APPEND) => DestinationOptions::Append,
         _ if matches.is_present(OVERWRITE) => DestinationOptions::Overwrite,
