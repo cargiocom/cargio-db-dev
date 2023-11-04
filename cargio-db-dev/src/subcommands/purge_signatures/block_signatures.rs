51de1f8b7be2baa82120fdb07c89f66af8daf46d
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-11-04 05:12:38 -0700

    Update block_signatures.rs

--- cargio-db-dev/src/subcommands/purge_signatures/block_signatures.rs
+++ cargio-db-dev/src/subcommands/purge_signatures/block_signatures.rs
@@ -3,20 +3,14 @@ use std::{
     fmt::{self, Display, Formatter},
 };
 
-use casper_node::types::BlockHash;
-use casper_types::{EraId, PublicKey, Signature};
+use master_node::types::BlockHash;
+use cargio_types::{EraId, PublicKey, Signature};
 use serde::{Deserialize, Serialize};
 
-/// A storage representation of finality signatures with the associated block
-/// hash. This structure had to be copied over from the node codebase because
-/// it is not publicly accessible through the API.
 #[derive(Clone, Debug, Default, PartialOrd, Ord, Hash, Serialize, Deserialize, Eq, PartialEq)]
 pub(crate) struct BlockSignatures {
-    /// The block hash for a given block.
     pub(crate) block_hash: BlockHash,
-    /// The era id for the given set of finality signatures.
     pub(crate) era_id: EraId,
-    /// The signatures associated with the block hash.
     pub(crate) proofs: BTreeMap<PublicKey, Signature>,
 }
 
